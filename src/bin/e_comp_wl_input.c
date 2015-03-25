0edb389cefa1227f9c023167751581c51d5f513f
Mike Blumenkrantz <zmike@osg.samsung.com>
2015-03-25 12:32:30 -0400

    decrement e_comp->new_clients counter when unsetting new_client flag on wl cursor clients
    Change-Id: If35c45cea17b947bbc3d6b320ee08fee24cd91af

--- src/bin/e_comp_wl_input.c
+++ src/bin/e_comp_wl_input.c
@@ -64,6 +64,7 @@ _e_comp_wl_input_pointer_cb_cursor_set(struct wl_client *client, struct wl_resou
         ec = e_client_new(NULL, e_pixmap_new(E_PIXMAP_TYPE_WL, sid), 1, 0);
         ec->lock_focus_out = ec->layer_block = ec->visible = ec->override = 1;
         ec->new_client = 0;
+        e_comp->new_clients--;
         ec->icccm.title = eina_stringshare_add("noshadow");
         ec->icccm.window_role = eina_stringshare_add("wl_pointer-cursor");
         evas_object_pass_events_set(ec->frame, 1);
