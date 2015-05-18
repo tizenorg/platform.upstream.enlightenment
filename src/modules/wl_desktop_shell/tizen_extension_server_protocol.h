7944731f2db76c30a4ca625ec409322a48b0770b
Duna Oh <duna.oh@samsung.com>
2015-05-18 18:34:00 +0900

    split 'tizen_transient_for' interface from tizen_surface_extension
    Change-Id: Ie59510f9e502878d2d8525821e9613b8a4756b8f
    Signed-off-by: Duna Oh <duna.oh@samsung.com>

--- src/modules/wl_desktop_shell/tizen_extension_server_protocol.h
+++ src/modules/wl_desktop_shell/tizen_extension_server_protocol.h
@@ -14,9 +14,11 @@ struct wl_resource;
 
 struct tizen_resource;
 struct tizen_surface_extension;
+struct tizen_transient_for;
 
 extern const struct wl_interface tizen_resource_interface;
 extern const struct wl_interface tizen_surface_extension_interface;
+extern const struct wl_interface tizen_transient_for_interface;
 
 struct tizen_resource_interface {
 	/**
@@ -47,15 +49,6 @@ struct tizen_surface_extension_interface {
 				   uint32_t id,
 				   struct wl_resource *surface);
 	/**
-	 * set_transient_for - (none)
-	 * @child_id: (none)
-	 * @parent_id: (none)
-	 */
-	void (*set_transient_for)(struct wl_client *client,
-				  struct wl_resource *resource,
-				  uint32_t child_id,
-				  uint32_t parent_id);
-	/**
 	 * place_below_parent - (none)
 	 * @subsurface: (none)
 	 */
@@ -64,6 +57,25 @@ struct tizen_surface_extension_interface {
 				   struct wl_resource *subsurface);
 };
 
+struct tizen_transient_for_interface {
+	/**
+	 * set - (none)
+	 * @child_id: (none)
+	 * @parent_id: (none)
+	 */
+	void (*set)(struct wl_client *client,
+		    struct wl_resource *resource,
+		    uint32_t child_id,
+		    uint32_t parent_id);
+};
+
+#define TIZEN_TRANSIENT_FOR_DONE	0
+
+static inline void
+tizen_transient_for_send_done(struct wl_resource *resource_, uint32_t child_id)
+{
+	wl_resource_post_event(resource_, TIZEN_TRANSIENT_FOR_DONE, child_id);
+}
 
 #ifdef  __cplusplus
 }
