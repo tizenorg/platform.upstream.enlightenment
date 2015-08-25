0f189b5d6548d3ddee66ecf1269dde703c873b41
SooChan Lim <sc1.lim@samsung.com>
2015-08-25 23:35:47 +0900

    e_comp_wl_tbm: add e_comp_wl_tbm
    first implementation for wayland-tbm server
    
    Change-Id: I06c44fb9211efaba208a1537af3c17360f7797fd

---
+++ src/bin/e_comp_wl_tbm.h
@@ -0,0 +1,11 @@
+#ifdef E_TYPEDEFS
+
+#else
+# ifndef E_COMP_WL_TBM_H
+#  define E_COMP_WL_TBM_H
+
+EINTERN Eina_Bool e_comp_wl_tbm_init(void);
+EINTERN void e_comp_wl_tbm_shutdown(void);
+
+# endif
+#endif
