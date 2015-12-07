4925578e4d8f60d345d2839b3a817793e2df3c09
Duna Oh <duna.oh@samsung.com>
2015-12-07 19:02:34 +0900

    e_comp_wl: check number of devices before enabling/disabling devices
    Signed-off-by: Duna Oh <duna.oh@samsung.com>
    
    Change-Id: I475428014b531c29c5388606596e833c28523183

--- src/bin/e_comp_wl.h
+++ src/bin/e_comp_wl.h
@@ -184,6 +184,7 @@ struct _E_Comp_Wl_Data
         int mod_changed;
         int repeat_delay;
         int repeat_rate;
+        unsigned int num_devices;
      } kbd;
 
    struct
@@ -193,12 +194,14 @@ struct _E_Comp_Wl_Data
         wl_fixed_t x, y;
         wl_fixed_t grab_x, grab_y;
         uint32_t button;
+        unsigned int num_devices;
      } ptr;
 
    struct
      {
         Eina_List *resources;
         Eina_Bool enabled : 1;
+        unsigned int num_devices;
      } touch;
 
    struct
