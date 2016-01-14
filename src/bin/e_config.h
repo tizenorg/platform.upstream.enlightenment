bf305c9f30dfa4f82d8706f6c29eebce789659cd
Łukasz Stelmach <l.stelmach@samsung.com>
2016-01-14 15:29:05 +0100

    Enable changing owner, group and permissions for /run/waylan-0
    Introduce three new configuration options to change the owner, group
    and permissions for the Wayland socket.
    
    Change-Id: Idd3cfdad8bad7b33e9359e05cb3566595fcee11e
    Signed-off-by: Łukasz Stelmach <l.stelmach@samsung.com>

--- src/bin/e_config.h
+++ src/bin/e_config.h
@@ -456,6 +456,10 @@ struct _E_Config
    int cursor_timer_interval;
 
    Eina_List *client_types;
+
+   const char*  wayland_socket_owner;
+   const char*  wayland_socket_group;
+   int          wayland_socket_permissions;
 };
 
 struct E_Config_Bindings
