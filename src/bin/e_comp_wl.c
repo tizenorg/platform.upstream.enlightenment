769c1b4fc157ab9a155357850649811b2458afa3
MinJeong Kim <minjjj.kim@samsung.com>
2016-02-16 20:48:43 +0900

    e_comp_wl: set NULL terminator for end of pname
    All string has to be terminated with NULL character.
    This patch fixed "use of uninitialized vaule" problem reported by
    valgrind.
    
    Change-Id: I5f2b558367a592376794823fe45f7542b2c680af
    Signed-off-by: MinJeong Kim <minjjj.kim@samsung.com>

--- src/bin/e_comp_wl.c
+++ src/bin/e_comp_wl.c
@@ -2970,10 +2970,9 @@ _e_comp_wl_pname_get(pid_t pid, char *name, int size)
 
    len = fread(pname, sizeof(char), 512, h);
    if (len > 0)
-     {
-        if ('\n' == pname[len - 1])
-          pname[len - 1] = '\0';
-     }
+     pname[len - 1] = '\0';
+   else
+     strncpy(pname, "NO NAME", sizeof(pname));
 
    fclose(h);
 
@@ -2994,10 +2993,9 @@ _e_comp_wl_pname_print(pid_t pid)
 
    len = fread(pname, sizeof(char), 512, h);
    if (len > 0)
-     {
-        if ('\n' == pname[len - 1])
-          pname[len - 1] = '\0';
-     }
+     pname[len - 1] = '\0';
+   else
+     strncpy(pname, "NO NAME", sizeof(pname));
 
    fclose(h);
 
