df262a2bd2d17b5ceb8ce6f90d85555ede0133c1
SooChan Lim <sc1.lim@samsung.com>
2016-03-04 11:43:58 +0900

    temporary : hwc on
    Change-Id: Ic58514c6166c4dc5e5799996e2c13d7792f5c6d2

--- src/bin/e_comp.c
+++ src/bin/e_comp.c
@@ -1269,7 +1269,8 @@ out:
 #endif
 
 #ifdef HAVE_HWC
-   if (conf->hwc)
+   // soolim : hwc default on...
+   //if (conf->hwc)
      {
         e_comp->hwc = e_comp_hwc_init();
         if (!e_comp->hwc)
