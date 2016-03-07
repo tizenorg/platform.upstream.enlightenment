72549eb608d2263f3efd0d332eceea8a87622bde
Gwanglim Lee <gl77.lee@samsung.com>
2016-03-07 11:41:22 +0900

    Removed hard-coded paths which are not used in tizen
    Change-Id: Iec37eb4b2597befafccfdd92d9ae531de0b248d2

--- src/bin/e_fm_device.c
+++ src/bin/e_fm_device.c
@@ -258,14 +258,6 @@ e_fm2_device_volume_add(E_Volume *v)
              v->mount_point = NULL;
              id = "disk";
              if ((v->uuid) && (v->uuid[0])) id = v->uuid;
-             if (ecore_file_is_dir("/media"))
-               snprintf(buf, sizeof(buf), "/media/%s", id);
-             else if (ecore_file_is_dir("/mnt"))
-               snprintf(buf, sizeof(buf), "/mnt/%s", id);
-             else if (ecore_file_is_dir("/tmp"))
-               snprintf(buf, sizeof(buf), "/tmp/%s", id);
-             else
-               buf[0] = 0;
              v->mount_point = eina_stringshare_add(buf);
           }
      }
