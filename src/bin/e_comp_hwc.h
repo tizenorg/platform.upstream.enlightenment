5a5ddc875164e1d5c5a3ebbbceda46cb566912e9
SooChan Lim <sc1.lim@samsung.com>
2016-03-22 13:25:10 +0900

    e_comp_hwc: first implemenation of the hwc
    Change-Id: I23d8f23f3ac48ecb05d59b82cf089af9b325c13e

---
+++ src/bin/e_comp_hwc.h
@@ -0,0 +1,12 @@
+#ifdef E_TYPEDEFS
+#else
+# ifndef E_COMP_HWC_H
+#  define E_COMP_HWC_H
+
+EINTERN Eina_Bool e_comp_hwc_init(void);
+EINTERN void      e_comp_hwc_shutdown(void);
+EINTERN Eina_Bool e_comp_hwc_mode_nocomp(E_Client *ec);
+EINTERN void      e_comp_hwc_display_client(E_Client *ec);
+
+# endif
+#endif
