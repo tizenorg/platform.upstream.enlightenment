de562ba9df2abb272b2c77f435caefb067564807
Roman Peresipkyn <r.peresipkyn@samsung.com>
2016-03-24 20:10:59 +0200

    wl_drm: create wayland output's globals for external output
    Change-Id: I63a59f36bc63c39bcf71e54ace0867bc9e2f3f5a
    Signed-off-by: Roman Peresipkyn <r.peresipkyn@samsung.com>

--- src/modules/wl_drm/e_mod_main.c
+++ src/modules/wl_drm/e_mod_main.c
@@ -55,9 +55,13 @@ end:
 static Eina_Bool
 _e_mod_drm_cb_output(void *data EINA_UNUSED, int type EINA_UNUSED, void *event)
 {
-   const Eina_List *l;
+   const Eina_List *l, *ll;
    E_Randr2_Screen *screen;
    Ecore_Drm_Event_Output *e;
+
+   Ecore_Drm_Device *dev;
+   Ecore_Drm_Output *o;
+
 
    if (!(e = event)) goto end;
 
@@ -84,6 +88,45 @@ _e_mod_drm_cb_output(void *data EINA_UNUSED, int type EINA_UNUSED, void *event)
 
              break;
           }
+     }
+
+   /* TODO: add #ifdef TIZEN_EXTERNAL_OUTPUT?
+    * As I understand we must notify wl_comp here that there has been
+    * added external output. wl_comp will create global object
+    * for that output and clients will be able to interact with one
+    * by wayland protocol
+    * Can We move that peace of code to e-mod-tizen-eom?
+    */
+   EINA_LIST_FOREACH(ecore_drm_devices_get(), l, dev)
+     {
+       EINA_LIST_FOREACH(dev->external_outputs, ll, o)
+         {
+            /* Temporary rake into account only HDMI-A-0 */
+            if (strcmp("HDMI-A-0", e->name))
+              {
+                 if (e->plug)
+                   {
+                      if (!e_comp_wl_output_init(e->name, e->make, e->model,
+                                                 e->x, e->y, e->w, e->h,
+                                                 e->phys_width, e->phys_height,
+                                                 e->refresh, e->subpixel_order,
+                                                 e->transform))
+                        {
+                           ERR("WL_DRM: Could not setup new output: %s", ecore_drm_output_model_get(o));
+                        }
+
+                      DBG("WL_DRM: Created external output: "
+                          "%s  x=%d y=%d  mode:%dx%d "
+                          "phys_size:%dx%d  refresh:%d\n",
+                          ecore_drm_output_model_get(o),
+                          e->x, e->y, e->w, e->h,
+                          e->phys_width, e->phys_height,
+                          e->refresh );
+                   }
+                 else
+                   e_comp_wl_output_remove(ecore_drm_output_model_get(o));
+              }
+         }
      }
 
    /* previous calculation of e_scale gave unsuitable value because
@@ -425,6 +468,7 @@ _drm_randr_create(void)
 
              s = E_NEW(E_Randr2_Screen, 1);
              if (!s) continue;
+
 
              s->info.name = ecore_drm_output_name_get(output);
              printf("DRM RRR: .... out %s\n", s->info.name);
