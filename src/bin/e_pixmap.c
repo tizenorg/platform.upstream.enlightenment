21da7a6da9f94e372b74bdedc3c8f23be7be4c05
Boram Park <boram1288.park@samsung.com>
2016-03-28 14:01:52 +0900

    tizen resource id should start from 1
       The tizen resource id is unsigned integer. And e_pixmap_res_id_get()
       returns 0 as error. Therefore, It doesn't make sence the tizen resource
       id starts from 0.
    
    Change-Id: Ia9d8e03536d9cc549884b530a91991e95f23cb95

--- src/bin/e_pixmap.c
+++ src/bin/e_pixmap.c
@@ -353,9 +353,8 @@ e_pixmap_new(E_Pixmap_Type type, ...)
 
          if (!res_ids)
            res_ids = eina_hash_int32_new(NULL);
-         cp->res_id = res_id;
+         cp->res_id = ++res_id;
          eina_hash_add(res_ids, &res_id, cp);
-         res_id++;
          ELOG("PIXMAP NEW", cp, cp->client);
          break;
 
