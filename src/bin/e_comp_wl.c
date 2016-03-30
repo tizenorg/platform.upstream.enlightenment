ea0ddd6b1cccc57f7e4844878d2ac79194f52830
MinJeong Kim <minjjj.kim@samsung.com>
2016-03-30 14:38:23 +0900

    Removed ecore_wl_server_mode_set
    Change-Id: I39298b58b68fadb2206aea66172e72cdfd452e6a
    Signed-off-by: MinJeong Kim <minjjj.kim@samsung.com>

--- src/bin/e_comp_wl.c
+++ src/bin/e_comp_wl.c
@@ -4251,8 +4251,6 @@ e_comp_wl_init(void)
         return EINA_FALSE;
      }
 
-   ecore_wl_server_mode_set(1);
-
    /* create hash to store client's buffer */
    clients_buffer_hash = eina_hash_pointer_new(NULL);
 
