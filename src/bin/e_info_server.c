9aabdd8adb1c8042c1d9d46c44a87f64fe0ecbd8
JengHyun Kang <jhyuni.kang@samsung.com>
2016-05-04 11:37:18 +0900

    Add a keymap command to enlightenment_info for query a current keymap
    Change-Id: Ic91b668f578c521e3fb62a54454f37c289aa352e

--- src/bin/e_info_server.c
+++ src/bin/e_info_server.c
@@ -757,6 +757,15 @@ _e_info_server_cb_rotation_message(const Eldbus_Service_Interface *iface EINA_UN
 }
 
 static Eldbus_Message *
+_e_info_server_cb_keymap_info_get(const Eldbus_Service_Interface *iface EINA_UNUSED, const Eldbus_Message *msg)
+{
+   Eldbus_Message *reply = eldbus_message_method_return_new(msg);
+
+   eldbus_message_arguments_append(reply, "hi", e_comp_wl->xkb.fd, e_comp_wl->xkb.size);
+   return reply;
+}
+
+static Eldbus_Message *
 _e_info_server_cb_fps_info_get(const Eldbus_Service_Interface *iface EINA_UNUSED, const Eldbus_Message *msg)
 {
    static double old_fps = 0;
@@ -1040,6 +1049,7 @@ static const Eldbus_Method methods[] = {
 #ifdef HAVE_HWC
    { "hwc_trace_message", ELDBUS_ARGS({"i", "hwc_trace_message"}), NULL, e_info_server_cb_hwc_trace_message, 0},
 #endif
+   { "get_keymap", NULL, ELDBUS_ARGS({"hi", "keymap fd"}), _e_info_server_cb_keymap_info_get, 0},
    { NULL, NULL, NULL, NULL, 0 }
 };
 
