3824422b08d654eb26d028bcc2ef19938ea53ba9
Juyeon Lee <juyeonne.lee@samsung.com>
2016-05-24 17:05:54 +0900

    refactoring hwc interface for extra pkg
    Change-Id: Ifb8e52c30e268b95890c920aab01d4e3d01e8697

--- src/bin/e_plane.h
+++ src/bin/e_plane.h
@@ -11,10 +11,17 @@ typedef struct _E_Plane                      E_Plane;
 typedef enum _E_Plane_Type_State
 {
    E_PLANE_TYPE_INVALID,
+   E_PLANE_TYPE_VIDEO,
    E_PLANE_TYPE_PRIMARY,
    E_PLANE_TYPE_OVERLAY,
    E_PLANE_TYPE_CURSOR
 } E_Plane_Type_State;
+
+typedef enum _E_Plane_Color
+{
+   E_PLANE_COLOR_YUV,
+   E_PLANE_COLOR_RGB
+} E_Plane_Color;
 
 struct _E_Plane
 {
@@ -23,8 +30,11 @@ struct _E_Plane
      {
         int          x, y, w, h; // FIXME
      } geometry;
+
    const char         *name;
    E_Plane_Type_State  type;
+   E_Plane_Color       color;
+
    E_Client           *ec;
    E_Client           *prepare_ec;
    E_Output           *eout;
@@ -32,17 +42,21 @@ struct _E_Plane
    Eina_Bool           is_primary;
 };
 
-extern E_API int E_EVENT_PLANE_ADD;
-extern E_API int E_EVENT_PLANE_DEL;
-
-EINTERN int    e_plane_init(void);
-EINTERN int    e_plane_shutdown(void);
-EINTERN E_Plane  * e_plane_new(E_Output *eout, int zpos);
-EINTERN void       e_plane_free(E_Plane *plane);
-E_API Eina_Bool  e_plane_resolution_set(E_Plane *plane, int w, int h);
-E_API void       e_plane_type_set(E_Plane *plane, E_Plane_Type_State type);
+EINTERN int              e_plane_init(void);
+EINTERN int              e_plane_shutdown(void);
+EINTERN E_Plane         *e_plane_new(E_Output *eout, int zpos);
+EINTERN void             e_plane_free(E_Plane *plane);
+E_API Eina_Bool          e_plane_resolution_set(E_Plane *plane, int w, int h);
+E_API void               e_plane_type_set(E_Plane *plane, E_Plane_Type_State type);
 E_API E_Plane_Type_State e_plane_type_get(E_Plane *plane);
-
+E_API E_Client          *e_plane_ec_get(E_Plane *plane);
+E_API E_Client          *e_plane_ec_prepare_get(E_Plane *plane);
+E_API Eina_Bool          e_plane_ec_prepare_set(E_Plane *plane, E_Client *ec);
+E_API const char        *e_plane_ec_prepare_set_last_error_get(E_Plane *plane);
+E_API Eina_Bool          e_plane_is_primary(E_Plane *plane);
+E_API Eina_Bool          e_plane_is_cursor(E_Plane *plane);
+E_API E_Plane_Color      e_plane_color_val_get(E_Plane *plane);
+E_API void               e_plane_geom_get(E_Plane *plane, int *x, int *y, int *w, int *h);
 
 #endif
 #endif
