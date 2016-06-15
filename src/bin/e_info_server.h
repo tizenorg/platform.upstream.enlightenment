74e878aca5fadee57e59b80f1511706061e4868e
JeongHyun Kang <jhyuni.kang@samsung.com>
2016-06-15 15:34:29 +0900

    enlightenment_info: Add module info and keygrab status   - Module info: If a module want to print logs, a module add a info callback                  and enlightenment_info call requested module's callback
    Change-Id: Id2237521e7574edae0310ededc93119c9a70542e

--- src/bin/e_info_server.h
+++ src/bin/e_info_server.h
@@ -1,4 +1,6 @@
 #ifdef E_TYPEDEFS
+typedef void (*E_Info_Hook_Cb)(void *data, const char *log_path);
+typedef struct _E_Info_Hook E_Info_Hook;
 #else
 #ifndef E_INFO_SERVER_H
 #define E_INFO_SERVER_H
@@ -15,6 +17,8 @@ struct E_Event_Info_Rotation_Message
 };
 
 E_API extern int E_EVENT_INFO_ROTATION_MESSAGE;
+
+E_API void e_info_server_hook_set(const char *module_name, E_Info_Hook_Cb func, void *data);
 
 EINTERN int e_info_server_init(void);
 EINTERN int e_info_server_shutdown(void);
@@ -44,6 +48,13 @@ struct wl_closure
 struct argument_details {
    char type;
    int nullable;
+};
+
+struct _E_Info_Hook
+{
+   const char *module_name;
+   E_Info_Hook_Cb func;
+   void *data;
 };
 
 #endif
