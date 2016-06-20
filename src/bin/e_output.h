0d1aadd11c439beac457f619b103bec2e10e18c7
Juyeon Lee <juyeonne.lee@samsung.com>
2016-06-20 15:46:41 +0900

    e_output: added e_output_is_fb_compositing
    one of output->planes is dedicated for fb target
    the plane is used by compositor for e_comp->evas compositing
    often, the plane can be assined by one surface if it meets hwc condition
    
    Change-Id: I458146060cae2ca0353e04762cc78ce3fb91116e

--- src/bin/e_output.h
+++ src/bin/e_output.h
@@ -69,6 +69,7 @@ EINTERN const Eina_List * e_output_screens_get(void);
 E_API E_Output          * e_output_find(const char *id);
 E_API const Eina_List   * e_output_planes_get(E_Output *eout);
 E_API void                e_output_util_planes_print(void);
+E_API Eina_Bool           e_output_is_fb_composing(E_Output *eout);
 
 #endif
 #endif
