8f85eb07d450c539d0e081cad6e6b2cac62c1928
Gwanglim Lee <gl77.lee@samsung.com>
2016-07-15 16:27:21 +0900

    e_screensaver: fixed that screensaver was still activated, even though key events were generated.
    The e_comp_canvas could stop propagation of key event in its key event
    handler if focused ec exists. In order to get notified of input event
    generation always, e_screensaver_notidle() has been added to key event
    handler of e_comp_canvas.
    
    Change-Id: Ide00295da07a58788281f6e36e48675a82c40c7c

--- src/bin/e_screensaver.h
+++ src/bin/e_screensaver.h
@@ -9,6 +9,7 @@ EINTERN int    e_screensaver_shutdown(void);
 
 E_API   void   e_screensaver_timeout_set(double time);
 E_API   double e_screensaver_timeout_get(void);
+E_API   void   e_screensaver_notidle(void);
 
 E_API extern int E_EVENT_SCREENSAVER_ON;
 E_API extern int E_EVENT_SCREENSAVER_OFF_PRE;
