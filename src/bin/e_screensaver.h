e5dbdbb877f8344222a4438629a1ff4789c6d708
Gwanglim Lee <gl77.lee@samsung.com>
2016-07-21 19:54:44 +0900

    e_screensaver: add support for enabling and disabling screensaver.
    Change-Id: I0ed2f522071022c84131fe7ed4359d09701f333a

--- src/bin/e_screensaver.h
+++ src/bin/e_screensaver.h
@@ -10,6 +10,8 @@ EINTERN int    e_screensaver_shutdown(void);
 E_API   void   e_screensaver_timeout_set(double time);
 E_API   double e_screensaver_timeout_get(void);
 E_API   void   e_screensaver_notidle(void);
+E_API   void   e_screensaver_enable(void);
+E_API   void   e_screensaver_disable(void);
 
 E_API extern int E_EVENT_SCREENSAVER_ON;
 E_API extern int E_EVENT_SCREENSAVER_OFF_PRE;
