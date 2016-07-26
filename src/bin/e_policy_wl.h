4b7e2c6721344dd4076dbf7e206777e54e0ee05e
MinJeong Kim <minjjj.kim@samsung.com>
2016-07-26 17:43:19 +0900

    e_policy: support sending event aux_message of tizen_policy
    Change-Id: Iaa0d7dbeeb11c3854c2a28ce1bd2912aed2ba66b

--- src/bin/e_policy_wl.h
+++ src/bin/e_policy_wl.h
@@ -29,6 +29,7 @@ void      e_policy_wl_win_scrmode_apply(void);
 
 /* aux_hint */
 void      e_policy_wl_aux_hint_init(void);
+void      e_policy_wl_aux_message_send(E_Client *ec, const char *key, const char *val, Eina_List *options);
 void      e_policy_wl_eval_pre_post_fetch(E_Client *ec);
 
 /* window brightness */
